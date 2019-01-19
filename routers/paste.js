1916daea584d13a8715330fe62814ef2464b74cd
Max Rumsey <22051776+dathappysheep@users.noreply.github.com>
2019-01-20 07:07:03 +0800

    Configuring the pastebin to work.

---
+++ routers/paste.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const nanoid = require('nanoid');
+
+exports.static = {
+  method: 'GET',
+  path: '/paste/{param*}',
+  handler: {
+    directory: {
+      path: './files',
+    }
+  }
+}
+
+exports.upload = {
+  method: 'POST',
+  path: '/paste',
+  options: {
+    handler: async (req, h) => {
+      const key = nanoid(24); // Relies on no accidental rewrites.
+
+      try {
+        await handleFileUpload(req.payload.paste, Buffer.from(key))
+      } catch (e) {
+        console.log(e);
+        return h.response('Internal Server Error').code(500);
+      }
+      return h.redirect(`/paste/${key}.txt`);
+    }
+  }
+}
+const handleFileUpload = (file, key) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./files/' + key + '.txt', file, err => {
+      if (err) {
+       reject(err)
+      }
+      resolve()
+   })
+ })
+}
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const server = Hapi.server({
 // Loads subrouters
 const files = [
   require('./routers/image.js'),
+  require('./routers/paste.js'),
   require('./routers/static.js'),
 ]
 
